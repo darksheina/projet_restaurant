59a020e4628bdd7dc5b405c8de8e0fb048f32841
Diewyns <Dupont.Axs@outlook.fr>
2018-12-11 11:23:46 +0100

    Butler Basic

--- ProjetRestaurant/RestaurantModel/DinerRoom/Buttler.cs
+++ ProjetRestaurant/RestaurantModel/DinerRoom/Buttler.cs
@@ -1,22 +1,43 @@
 using System;
+using System.Collections.Generic;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
 namespace RestaurationModel.DinerRoom
 {
 	public class Buttler {
-		public void GetTableStatus(list<Model.Salle de restauration.Table> tableList) {
-			throw new System.Exception("Not implemented");
+		
+        public List<Table.TableStatus> GetTableStatus(List<Table.TableStatus> tableList)
+        {
+            List<Table.TableStatus> statusAllTable = null;
+            for (int i = 0; i < tableList.Count; i++)
+            {
+                statusAllTable[i] = tableList[i];
+            }
+            return statusAllTable;
+        }
+
+		public int GetCustomerGroupNumber(CustomerGroup customerGroup)
+        {
+            return customerGroup.GroupNumber;
+        }
+
+		public void TableAttribution(CustomerGroup group, Table table)
+        {
+            // Methode à ameliorer pour un meilleur choix des tables
+            if (group.GroupNumber <= table.SeatNumber)
+            {
+                group.IDTable = table.IDTable;
+            }
+            else if (group.GroupNumber >= table.SeatNumber)
+            {
+                Console.WriteLine("Autre table requise");
+            }
 		}
-		public void GetCustomerGroupNumber() {
-			throw new System.Exception("Not implemented");
-		}
-		public void TableAttribution(int groupID, int iDTable) {
-			throw new System.Exception("Not implemented");
-		}
+
 		public void CallHeadWaiter() {
 			throw new System.Exception("Not implemented");
 		}
-
 		private Person person;
-
 	}
 
 }
--- ProjetRestaurant/RestaurantModel/DinerRoom/CustomerGroup.cs
+++ ProjetRestaurant/RestaurantModel/DinerRoom/CustomerGroup.cs
@@ -1,15 +1,16 @@
 using System;
+using System.Collections.Generic;
+
 namespace RestaurationModel.DinerRoom
 {
 	public class CustomerGroup {
 		public int GroupNumber;
-		public List<Model.Kitchen.Food> GroupOrder;
+		public List<Kitchen.Food> GroupOrder;
 		public int IDTable;
 		public int GroupID;
-		public EatStrategy EatStrategy;
+		public EatStategy eatStrategy;
 
 		public void AskBread() {
-			throw new System.Exception("Not implemented");
 		}
 		public void AskWater() {
 			throw new System.Exception("Not implemented");
@@ -29,17 +30,15 @@ namespace RestaurationModel.DinerRoom
 		public void SetGroupID(int groupID) {
 			this.GroupID = groupID;
 		}
-		public void<Model.Kitchen.Food> GetGroupOrder() {
-			throw new System.Exception("Not implemented");
+		public List<Kitchen.Food> GetGroupOrder() {
+            return this.GroupOrder;
 		}
-		public void SetGroupOrder(object<Model.Kitchen.Food> groupOrder) {
-			throw new System.Exception("Not implemented");
+		public void SetGroupOrder(List<Kitchen.Food> groupOrder) {
+            this.GroupOrder = groupOrder;
 		}
 
 		private EatStategy eatStategy;
-
 		private Person person;
-
 	}
 
 }
