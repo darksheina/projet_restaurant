a2ab177e6b730ca814f1f2ac827305ba5328232c
VincSP <vincent.serepeyrigain@viacesi.fr>
2018-12-12 15:37:16 +0100

    datagrid exp
    tentative de récupération des données du headwaiter pour la datagrid (vue)

--- ProjetRestaurant/RestaurationView/MainWindow.xaml.cs
+++ ProjetRestaurant/RestaurationView/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ProjetLibrary.Business.RestaurantDinerRoom.DinerRoom;
 
 namespace RestaurationView
 {
@@ -23,6 +24,20 @@ namespace RestaurationView
         public MainWindow()
         {
             InitializeComponent();
+            List<HeadWaiter> roleBuisinesses = new List<HeadWaiter>();
+            HeadWaiter headwait = new HeadWaiter();
+            headwait.RankAssign = 2;
+            headwait.SquareAssign = 4;
+            headwait.isBusy = true;
+            HeadWaiter headwait2 = new HeadWaiter();
+            headwait2.RankAssign = 4;
+            headwait2.SquareAssign = 2;
+            headwait2.isBusy = false;
+
+
+            roleBuisinesses.Add(headwait);
+            roleBuisinesses.Add(headwait2);
+            RestaurantActor.ItemsSource = roleBuisinesses;
         }
     }
 }
