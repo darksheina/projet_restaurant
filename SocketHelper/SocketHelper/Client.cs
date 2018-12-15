7d2689b68a33215f5b0c679e89f29aa9070d6f9e
darksheina <darksheina@hotmail.fr>
2018-12-16 00:38:07 +0100

    Sockets !!!
    dll socket client and server
    example with TestServer --> chef de cuisine
      TestClient --> waiter

---
+++ SocketHelper/SocketHelper/Client.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketHelper
+{
+    public class Client
+    {
+        string serverIP;
+        int port;
+
+        public Client(string serverIP, int port)
+        {
+            this.serverIP = serverIP;
+            this.port = port;
+        }
+
+        public void SentMessage(Message message)
+        {
+            string msgJson = JsonConvert.SerializeObject(message);
+            TcpClient client = new TcpClient(serverIP, port);
+
+            int byteCount = Encoding.ASCII.GetByteCount(msgJson);
+
+            byte[] sendData = new byte[byteCount];
+
+            sendData = Encoding.ASCII.GetBytes(msgJson);
+
+            NetworkStream stream = client.GetStream();
+
+            stream.Write(sendData, 0, sendData.Length);
+
+            stream.Close();
+            client.Close();
+        }
+    }
+}
