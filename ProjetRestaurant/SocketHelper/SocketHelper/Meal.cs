130a2148f37b7cb2fc1a9d4429fc100139d59e8a
laure_anne <laureanne.marechal@viacesi.fr>
2018-12-16 20:35:05 +0100

    Sockets

---
+++ ProjetRestaurant/SocketHelper/SocketHelper/Meal.cs
@@ -0,0 +1,17 @@
+﻿
+namespace SocketHelper
+{
+    public class Meal
+    {
+        public Meal(string starter, string dish, string dessert)
+        {
+            this.starter = starter;
+            this.dish = dish;
+            this.dessert = dessert;
+        }
+
+        public string starter;
+        public string dish;
+        public string dessert;
+    }
+}
---
+++ ProjetRestaurant/SocketHelper/SocketHelper/Message.cs
@@ -0,0 +1,13 @@
+﻿
+namespace SocketHelper
+{
+    public class Message
+    {
+        public string action;
+        public int rank;
+        public int table;
+        public Meal[] meals;
+        public string senderIp;
+        public int senderPort;
+    }
+}
